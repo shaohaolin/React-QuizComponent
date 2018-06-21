b6371b3b16963f233b9cf8c1347ebd5101bd5f32
Shaohao <shaohaolinca@gmail.com>
2018-06-21 15:47:42 -0700

    error message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,18 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
 
+    constructor(props){
+        super(props)
+        this.state = {incorrectAnswer: false}
+    }
+
     handleClick(buttonText){
         if (buttonText === this.props.quiz_question.answer) {
+            this.setState({incorrectAnswer: false})
             this.props.showNextQuestionHandler();
+        }
+        else {
+            this.setState({incorrectAnswer: true})
         }
     }
 
@@ -23,6 +32,7 @@ class QuizQuestion extends Component {
                         })}
                     </ul>
                 </section>
+                <p className='error'>{this.state.incorrectAnswer ? "Sorry, that's not right." : null}</p>
           </main>
         )
     }
